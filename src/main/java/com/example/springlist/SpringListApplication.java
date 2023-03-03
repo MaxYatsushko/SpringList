16dea0453afc7640c7bf165d15f9db8d1e47a81a
Maks <max@moais.ru>
2023-03-03 18:51:02 +0300

    Initial commit

---
+++ src/main/java/com/example/springlist/SpringListApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringListApplication.class, args);
+    }
+
+}
